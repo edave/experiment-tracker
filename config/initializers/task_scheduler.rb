c69b424daf8b9fc38a011003edb2a9bcbcacaffa
David Pitman <edave@mit.edu>
2009-12-14 10:44:23 -0500

    Removing special reminder task

--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -4,7 +4,7 @@ def scheduler.handle_exception (job, exception)
     Rails.logger.warn "#{Time.now} :: Rufus Scheduler :: job #{job.job_id} caught exception\n#{exception.message}\n#{exception.backtrace.join("\n")}"
   end
 
-scheduler.cron '0 22 * * *', :timeout => '30m' do
+scheduler.cron '0 21 * * *', :timeout => '30m' do
    Rails.logger.info "#{Time.now} :: Starting participant notifier task..."
    slots = Slot.find_by_occupied.find_by_day(Date.tomorrow)
    for slot in slots
@@ -14,17 +14,7 @@ scheduler.cron '0 22 * * *', :timeout => '30m' do
    
 end
 
-scheduler.in "5s" do #cron '0 22 * * *' do
-   Rails.logger.info "#{Time.now} :: Special Starting participant notifier task..."
-   slots = Slot.find_by_occupied.find_by_day(Date.today)
-   for slot in slots
-     SlotNotifier.deliver_reminder(slot)
-    end
- Rails.logger.info "#{Time.now} :: Special Participant notifier task finished: #{slots.length} notified"
-   
-end 
-
-scheduler.cron '0 23 * * *', :timeout => '30m' do
+scheduler.cron '0 22 * * *', :timeout => '30m' do
   Rails.logger.info "#{Time.now} :: Starting experiment schedule notifier task..."
   experiments = Experiment.find(:all)
   day = Date.tomorrow
