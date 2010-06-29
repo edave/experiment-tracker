0cced6ac42e9a4f1abd33d09b1f4e0d23b49cbd8
David Pitman <edave@mit.edu>
2010-06-29 13:16:46 -0400

    Possible bugfix for forgery protection

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -26,13 +26,5 @@ module ApplicationHelper
     end
     return ""
   end
-  
-    def request_forgery_protection_tag
-    @request_forgery_protection_tag ||= \
-      tag(:input, :type => "hidden", 
-          :name => request_forgery_protection_token.to_s, 
-          :value => form_authenticity_token,
-          :id => 'authenticity_token') \
-      if protect_against_forgery?
-  end
+
 end
